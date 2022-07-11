767878008fae55ce5a8da86f87341d191cac31da
Blancamc00 <blanca.manuc@gmail.com>
2022-07-11 11:22:15 +0100

    Queen moves

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -10,7 +10,49 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var moves = new List<Square>();
+            Square pos = board.FindPiece(this);
+            Square latestMove = pos;
+
+            while (latestMove.Col < GameSettings.BoardSize-1 && latestMove.Row < GameSettings.BoardSize-1)
+            {
+                latestMove = Square.At(latestMove.Row + 1, latestMove.Col + 1);
+                moves.Add(latestMove);
+            }
+            
+            latestMove = pos;
+            
+            while (latestMove.Col < GameSettings.BoardSize-1 && latestMove.Row > 0)
+            {
+                latestMove = Square.At(latestMove.Row-1, latestMove.Col+1);
+                moves.Add(latestMove);
+            }
+            
+            latestMove = pos;
+            
+            while (latestMove.Col > 0 && latestMove.Row > 0)
+            {
+                latestMove = Square.At(latestMove.Row-1, latestMove.Col-1);
+                moves.Add(latestMove);
+            }
+            latestMove = pos;
+            
+            while (latestMove.Col > 0 && latestMove.Row < GameSettings.BoardSize-1 )
+            {
+                latestMove = Square.At(latestMove.Row+1, latestMove.Col-1);
+                moves.Add(latestMove);
+            }
+            
+            for (var i = 0; i < GameSettings.BoardSize; i++)
+                moves.Add(Square.At(pos.Row, i));
+
+            for (var i = 0; i < GameSettings.BoardSize; i++)
+                moves.Add(Square.At(i, pos.Col));
+
+            //Get rid of our starting location.
+            moves.RemoveAll(s => s == pos);
+            
+            return moves;
         }
     }
 }
