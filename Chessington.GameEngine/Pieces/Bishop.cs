71031130a8c6d1f1bb478339aaf41fbb12b8f186
Blancamc00 <blanca.manuc@gmail.com>
2022-07-11 11:17:42 +0100

    Bishop moves

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -10,7 +10,41 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var moves = new List<Square>();
+            Square pos = board.FindPiece(this);
+            Square latestMove = pos;
+
+            while (latestMove.Col < 7 && latestMove.Row < 7)
+            {
+                latestMove = Square.At(latestMove.Row + 1, latestMove.Col + 1);
+                moves.Add(latestMove);
+            }
+            
+            latestMove = pos;
+            
+            while (latestMove.Col < 7 && latestMove.Row > 0)
+            {
+                latestMove = Square.At(latestMove.Row-1, latestMove.Col+1);
+                moves.Add(latestMove);
+            }
+            
+            latestMove = pos;
+            
+            while (latestMove.Col > 0 && latestMove.Row > 0)
+            {
+                latestMove = Square.At(latestMove.Row-1, latestMove.Col-1);
+                moves.Add(latestMove);
+            }
+            latestMove = pos;
+            
+            while (latestMove.Col > 0 && latestMove.Row < 7 )
+            {
+                latestMove = Square.At(latestMove.Row+1, latestMove.Col-1);
+                moves.Add(latestMove);
+            }
+
+
+            return moves;
         }
     }
 }
